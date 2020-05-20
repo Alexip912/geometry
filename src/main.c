7f3c374225b68293c21f16c70b4f1b7d496d5363
Alexip912 <55093283+Alexip912@users.noreply.github.com>
2020-05-20 23:52:53 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,50 +1,62 @@
 #include "area.h"
+#include "check.h"
 #include "crossing.h"
 #include "fname.h"
 #include "perimeter.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     char figure[30];
     int x1, y1, x2, y2;
     int r1, r2;
+    char sx1[10], sy1[10], sx2[10], sy2[10];
+    char sr1[10], sr2[10];
     int s1, s2, p1, p2;
     printf("Название фигуры - ");
     scanf("%s", figure);
     int flag = figure_name(figure);
     if (flag == 0) {
         printf("Введите координаты первой окружности - ");
-        if (scanf("%d", &x1) != 1) {
+        scanf("%s", sx1);
+        if (is_valid_number(sx1) == -1) {
             printf("Надо вводить число");
             return 0;
         }
-        if (scanf("%d", &y1) != 1) {
+        scanf("%s", sy1);
+        if (is_valid_number(sy1) == -1) {
             printf("Надо вводить число");
             return 0;
         }
         printf("Введите радиус первой окружности: ");
-        if (scanf("%d", &r1) != 1) {
+        scanf("%s", sr1);
+        if (is_valid_number(sr1) == -1) {
             printf("Надо вводить число");
             return 0;
         } else {
+            r1 = atof(sr1);
             s1 = area(r1);
             p1 = perimeter(r1);
         }
         printf("Введите координаты второй окружности - ");
-        if (scanf("%d", &x2) != 1) {
+        scanf("%s", sx2);
+        if (is_valid_number(sx2) == -1) {
             printf("Надо вводить число");
             return 0;
         }
-        if (scanf("%d", &y2) != 1) {
+        scanf("%s", sy1);
+        if (is_valid_number(sy1) == -1) {
             printf("Надо вводить число");
             return 0;
         }
         printf("Введите радиус первой окружности: ");
-        if (scanf("%d", &r2) != 1) {
+        scanf("%s", sr2);
+        if (is_valid_number(sr2) == -1) {
             printf("Надо вводить число");
             return 0;
         } else {
+            r2 = atof(sr2);
             s2 = area(r2);
             p2 = perimeter(r2);
         }
@@ -56,6 +68,10 @@ int main()
         printf("Неверное значение радиуса");
         return 0;
     } else {
+        x1 = atof(sx1);
+        x2 = atof(sx2);
+        y1 = atof(sy1);
+        y2 = atof(sy2);
         int check = crossing(x1, x2, y1, y2, r1, r2);
         if (check != 0) {
             printf("\n<<Окружности не пересекаются>>\n");

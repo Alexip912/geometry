13618c001cf3465941454c6b8cf8310c263820c9
Alexip912 <55093283+Alexip912@users.noreply.github.com>
2020-05-21 01:24:11 +0700

    create is_valid_cord and change is_valid_radius

--- src/check.c
+++ src/check.c
@@ -1,9 +1,25 @@
 #include <ctype.h>
 #include <string.h>
 
-int is_valid_number(char* number)
+int is_valid_radius(char* number)
 {
     for (int i = 0; i < strlen(number); ++i) {
+        if (!isdigit(number[i])) {
+            return -1;
+        }
+    }
+    if (strcmp(number, "") == 0) {
+        return -1;
+    }
+    return 1;
+}
+
+int is_valid_cord(char* number)
+{
+    for (int i = 0; i < strlen(number); ++i) {
+        if (number[i] == '-') {
+            ++i;
+        }
         if (!isdigit(number[i])) {
             return -1;
         }
